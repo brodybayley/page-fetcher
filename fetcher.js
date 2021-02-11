81a840260d75d4e58013e94d699e2d711da05b84
Brody Bayley <brodybayley@gmail.com>
2021-02-11 05:39:23 +0000

    pushing through updates to function with final console

--- fetcher.js
+++ fetcher.js
@@ -1,18 +1,14 @@
-const message = process.argv.slice(2)
+const message = process.argv.slice(2);
 const URL = message[0];
-const filePath = message[1]
-const http = require('http');
-const fs = require('fs')
-const stats = fs.statSync(filePath)
-let bytes = stats.size;
+const filePath = message[1];
+const fs = require('fs');
 
 
 const request = require('request');
 request(URL, (error, response, body) => {
-  fs.writeFile(filePath, body, (error) => {
-    console.log(`Download complete! Saved ${body.length} bytes to ${filePath}`)
+  fs.writeFile(filePath, body, function (error) {
+    let data = body.length;
+    console.log(`Download complete! Saved ${data} bytes to ${filePath}`);
   });
 });
 
-
-// module.exports = { download };
