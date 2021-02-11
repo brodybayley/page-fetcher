e7f76d9018b8a3ee4ef5609f12e4b1dddca43a5e
Brody Bayley <brodybayley@gmail.com>
2021-02-11 05:27:05 +0000

    updates to function and added callback

--- fetcher.js
+++ fetcher.js
@@ -6,22 +6,13 @@ const fs = require('fs')
 const stats = fs.statSync(filePath)
 let bytes = stats.size;
 
-console.log(`Download complete! Saved ${stats.size} bytes to ${filePath}`);
 
 const request = require('request');
-request(URL, (request) => {
-  console.log('bytes:', stats); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
+request(URL, (error, response, body) => {
+  fs.writeFile(filePath, body, (error) => {
+    console.log(`Download complete! Saved ${body.length} bytes to ${filePath}`)
+  });
 });
 
-// request(URL, ())
-//         request.head(URL, (err, res, body) => {
-//         console.log('content-type:', res.headers['content-type']);
-//         console.log('content-length:', res.headers['content-length']);
-
-//         request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-//     });
-// };
 
 // module.exports = { download };
