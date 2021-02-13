01683260b774b5706bb5083f5ffe4bc168c80d11
Brody Bayley <brodybayley@gmail.com>
2021-02-13 00:59:37 +0000

    added error messages and wanted to also note that worked on this challenge with Ray

--- fetcher.js
+++ fetcher.js
@@ -6,7 +6,15 @@ const fs = require('fs');
 
 const request = require('request');
 request(URL, (error, response, body) => {
-  fs.writeFile(filePath, body, function (error) {
+  if (error) {
+    console.log('error:', error);
+    return;
+  }
+  fs.writeFile(filePath, body, function(error) {
+    if (error) {
+      console.log('error:', error);
+      return;
+    }
     let data = body.length;
     console.log(`Download complete! Saved ${data} bytes to ${filePath}`);
   });
